8d15547412cf110a449f99386924bfa6d1bfc858
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2024-09-20 08:35:45 +0700

    Praktikum 3: Soal No. 3.2

--- Pertemuan4/operator.php
+++ Pertemuan4/operator.php
@@ -15,4 +15,18 @@ echo "Hasil perkalian {$a} * {$b} = {$hasilKali} <br>";
 echo "Hasil pembagian {$a} / {$b} = {$hasilBagi} <br>";
 echo "Sisa pembagian {$a} % {$b} = {$sisaBagi} <br>";
 echo "Hasil pemangkatan {$a} ** {$b} = {$pangkat} <br>";
+
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
+echo "Apakah {$a} sama dengan {$b}? " . ($hasilSama ? 'Ya' : 'Tidak') . "<br>";
+echo "Apakah {$a} tidak sama dengan {$b}? " . ($hasilTidakSama ? 'Ya' : 'Tidak') . "<br>";
+echo "Apakah {$a} lebih kecil dari {$b}? " . ($hasilLebihKecil ? 'Ya': 'Tidak') . "<br>";
+echo "Apakah {$a} lebih besar dari {$b}? " . ($hasilLebihBesar ? 'Ya' : 'Tidak') . "<br>";
+echo "Apakah {$a} lebih kecil atau sama dengan {$b}? " . ($hasilLebihKecilSama ? 'Ya' : 'Tidak') . "<br>";
+echo "Apakah {$a} lebih besar atau sama dnegan {$b}? " . ($hasilLebihBesarSama ? 'Ya' : 'Tidak') . "<br>";
 ?>
