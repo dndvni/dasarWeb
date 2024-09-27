ea7e92cd8790d9e6ef52c4fa6d65128ce3fa63f5
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2024-09-27 09:27:45 +0700

    Praktikum 1: Soal No. 5

--- Pertemuan5/fungsi.php
+++ Pertemuan5/fungsi.php
@@ -1,26 +1,17 @@
 <?php
 
-function perkenalan($nama, $salam="Assalamualaikum") {
-    echo $salam.", ";
-    echo "Perkenalkan, nama saya ".$nama."<br/>";
-    echo "Senang berkenalan dengan Anda<br/>";
-}
-
-perkenalan("Hamdana", "Hallo");
-
-echo "<hr>";
-
-$saya = "Devani";
-$ucapanSalam = "Selamat pagi";
-
-perkenalan($saya);
-
-// fungsi mengembalikan nilai
 function hitungUmur($thn_lahir, $thn_sekarang) {
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
 }
+function perkenalan ($nama, $salam="Assalamulaiakum ") {
+    echo $salam.",";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
 
-echo "Umur saya adalah ". hitungUmur(2004, 2024) ."tahun<br/>"
+    echo "saya berusia ". hitungUmur(2004, 2024) ." tahun</br>";
+    echo "Senang berkenalan dengan anda</br>";
+}
 
- ?> 
+perkenalan ("Devani");
+
+?>
