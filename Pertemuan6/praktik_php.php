9867c5d27d175cea43a997a546f807110f858ce1
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2024-10-04 08:28:18 +0700

    Fungsi dengan Parameter: Soal No. 5

--- Pertemuan6/praktik_php.php
+++ Pertemuan6/praktik_php.php
@@ -1,12 +1,29 @@
 <?php
 
-function perkenalan() {
-    echo "Assalamualaikum, ";
-    echo "Perkenalan , nama saya Devani</br>";
-    echo "Senang berkenalan dengan Anda</br>";
+// function perkenalan() {
+    // echo "Assalamualaikum, ";
+    // echo "Perkenalan , nama saya Devani</br>";
+    // echo "Senang berkenalan dengan Anda</br>";
+// }
+
+// memanggil fungsi yang sudah dibuat
+// perkenalan();
+
+// membuat fungsi
+function perkenalan($nama, $salam) {
+    echo $salam. ", ";
+    echo "Perkenalkan, nama saya ". $nama."<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
 }
 
 // memanggil fungsi yang sudah dibuat
-perkenalan();
+perkenalan("Hamdana", "Hallo");
 
+echo "<hr>";
+
+$saya = "Devani";
+$ucapanSalam = "Selamat pagi";
+
+// memanggil lagi
+perkenalan($saya, $ucapanSalam);
 ?>
