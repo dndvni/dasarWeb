06bb7294aa506fc3827da8ea2acd76caa7f94424
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2024-10-11 09:02:38 +0700

    Function Empty: Soal No. 2,2

--- Pertemuan7/empty.php
+++ Pertemuan7/empty.php
@@ -5,3 +5,9 @@ if (empty($myArray)) {
 } else {
     echo "Array terdefinisi dan tidak kososng.";
 }
+
+if(empty($nonExtentVar)) {
+    echo "Variabel tidak terdefinisi atau kosong.";
+} else {
+    echo "Variabel terdefinisi dan tidak kosong.";
+}
