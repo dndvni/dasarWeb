94c43dd5f380f4010f681ed4bfa3347f3eab3875
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2024-10-15 18:52:35 +0700

    Form Lanjut : Soal No. 6.1

---
+++ Pertemuan7/proses_lanjut.php
@@ -0,0 +1,23 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $selectedBuah = $_POST['buah'];
+
+    if (isset($_POST['warna'])) {
+        $selectedwarna = $_POST['warna'];
+    } else {
+        $selectedwarna = [];
+    }
+
+
+    $selectedJenisKelamin = $_POST['jenis_kelamin'];
+
+    echo "Anda memilih buah: " . $selectedBuah . "<br>";
+
+    if (!empty($selectedwarna)) {
+        echo "Warna favorit Anda: " . implode(", ", $selectedwarna) . "<br>";
+    } else {
+        echo "Anda tidak memilih warna favorit.<br>";
+    }
+    echo "Jenis kelamin Anda: " . $selectedJenisKelamin;
+}
