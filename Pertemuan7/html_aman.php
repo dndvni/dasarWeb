522bded8e5b95a5b3951a22289cff8f9f55fca20
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2024-10-15 17:09:28 +0700

    HTML Injection: Soal No. 4.2

--- Pertemuan7/html_aman.php
+++ Pertemuan7/html_aman.php
@@ -6,4 +6,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     echo "Nama: " . $input ."<br>";
 }
+
+// soal no 4.2 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Email: " . htmlspecialchars($email, ENT_QUOTES, 'UTF-8') . "</br>";
+    } else {
+        echo "Email tidak valid. <br>";
+    }
+}
+
 ?>
