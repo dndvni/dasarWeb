aa7de57cd1d274cdbec166f6a414f7ac46ea2727
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2024-10-18 11:49:34 +0700

    Upload Form : Soal No. 6 dan 7

--- Pertemuan8/upload.php
+++ Pertemuan8/upload.php
@@ -4,8 +4,8 @@
         $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
         $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-        $allowedExtensions = array("jpg", "jpeg", "png", "gif");
-        $maxsize = 5*1024*1024;
+        $allowedExtensions = array("txt", "pdf", "doc", "docx");
+        $maxsize = 3*1024*1024;
 
         if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
             
@@ -14,7 +14,7 @@
             echo "File berhasil diunggah<br>";
 
             // Soal No. 5
-            echo "<img src='".$targetfile."' width='200' />";
+            // echo "<img src='".$targetfile."' width='200' />";
         } else {
             echo "Gagal mengunggah file";
         }
