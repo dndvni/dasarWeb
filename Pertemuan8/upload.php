7101f55e943c64f590582aa23bf40bb5e712f508
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2024-10-18 11:33:56 +0700

    Upload Form : Soal No. 3 dan 4

--- Pertemuan8/upload.php
+++ Pertemuan8/upload.php
@@ -1,20 +1,24 @@
-<?php
-if(isset($_POST["submit"])) {
-    $targetdir = "uploads/";
-    $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
-    $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
+<?php 
+    if (isset($_POST["submit"])) {
+        $targetdir = "uploads.php";
+        $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
+        $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
-    $maxsize = 5*1024*1024;
+        $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+        $maxsize = 5*1024*1024;
 
-    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"]<=$maxsize) {
-        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targerfile)) {
-            echo "File berhasil diunggah.";
+        if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
+            
+        
+        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
+            echo "File berhasil diunggah<br>";
+
+            
         } else {
-            echo "Gagal mengunggah file.";
+            echo "Gagal mengunggah file";
         }
-    } else {
+    } else{
         echo "File tidak valid atau melebihi ukuran maksismum yang diizinkan";
     }
-}
+    }
 ?>
